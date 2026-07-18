627708a13113ac42f94f92f102af34c55c0898ba
Jeferson Santos <jefersonrouue@gmail.com>
2026-07-18 16:06:02 -0300

    feat : crud complete of categories

--- src/menu/categories/categories.controller.ts
+++ src/menu/categories/categories.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from '../categories/create-category.dto';
+import { UpdateCategoryDto } from '../categories/update-category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(
+    private readonly categoriesService: CategoriesService,
+  ) {}
+
+  /////////////////////////////////////////////////////
+  // CRIAR CATEGORIA
+  /////////////////////////////////////////////////////
+
+  @Post()
+  async criarCategoria(
+    @Body() dto: CreateCategoryDto,
+  ) {
+    const category =
+      await this.categoriesService.criarCategoria(dto);
+
+    return {
+      message: 'Categoria cadastrada com sucesso.',
+      data: category,
+    };
+  }
+
+  /////////////////////////////////////////////////////
+  // BUSCAR POR ID
+  /////////////////////////////////////////////////////
+
+  @Get(':id')
+  async buscarCategoriaById(
+    @Param('id') id: string,
+  ) {
+    const category =
+      await this.categoriesService.buscarCategoriaById(id);
+
+    return {
+      message: 'Categoria encontrada com sucesso.',
+      data: category,
+    };
+  }
+
+  /////////////////////////////////////////////////////
+  // LISTAR TODAS
+  /////////////////////////////////////////////////////
+
+  @Get()
+  async listarCategorias() {
+    const categories =
+      await this.categoriesService.listarCategorias();
+
+    return {
+      message: 'Categorias listadas com sucesso.',
+      data: categories,
+    };
+  }
+
+  /////////////////////////////////////////////////////
+  // ATUALIZAR
+  /////////////////////////////////////////////////////
+
+  @Patch(':id')
+  async atualizarCategoria(
+    @Param('id') id: string,
+    @Body() dto: UpdateCategoryDto,
+  ) {
+    const category =
+      await this.categoriesService.atualizarCategoria(
+        id,
+        dto,
+      );
+
+    return {
+      message: 'Categoria atualizada com sucesso.',
+      data: category,
+    };
+  }
+}
--- src/menu/categories/categories.service.ts
+++ src/menu/categories/categories.service.ts
@@ -1,4 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from '../categories/create-category.dto';
+import { UpdateCategoryDto } from '../categories/update-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private prisma: PrismaService) {}
+
+  /////////////////////////////////////////////////////
+  // CRIAR CATEGORIA
+  /////////////////////////////////////////////////////
+
+  async criarCategoria(dto: CreateCategoryDto) {
+    const categoryExists = await this.prisma.category.findUnique({
+      where: {
+        name: dto.name,
+      },
+    });
+
+    if (categoryExists) {
+      throw new ConflictException('Categoria já cadastrada.');
+    }
+
+    return await this.prisma.category.create({
+      data: dto,
+    });
+  }
+
+  /////////////////////////////////////////////////////
+  // BUSCAR POR ID
+  /////////////////////////////////////////////////////
+
+  async buscarCategoriaById(id: string) {
+    const category = await this.prisma.category.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Categoria não encontrada.');
+    }
+
+    return category;
+  }
+
+  /////////////////////////////////////////////////////
+  // LISTAR TODAS
+  /////////////////////////////////////////////////////
+
+  async listarCategorias() {
+    return await this.prisma.category.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    });
+  }
+
+  /////////////////////////////////////////////////////
+  // ATUALIZAR
+  /////////////////////////////////////////////////////
+
+  async atualizarCategoria(
+    id: string,
+    dto: UpdateCategoryDto,
+  ) {
+    await this.buscarCategoriaById(id);
+
+    if (dto.name) {
+      const exists = await this.prisma.category.findFirst({
+        where: {
+          name: dto.name,
+          NOT: {
+            id,
+          },
+        },
+      });
+
+      if (exists) {
+        throw new ConflictException(
+          'Já existe uma categoria com esse nome.',
+        );
+      }
+    }
+
+    return await this.prisma.category.update({
+      where: {
+        id,
+      },
+      data: dto,
+    });
+  }
+}
