cf41b51adb9711a2dac06488606d29da2061b69e
Jeferson Santos <jefersonrouue@gmail.com>
2026-07-18 16:39:52 -0300

    feat : implemented dash suummary orders

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(
+    private readonly dashboardService: DashboardService,
+  ) {}
+
+  /////////////////////////////////////////////////////
+  // RESUMO DO DASHBOARD
+  /////////////////////////////////////////////////////
+
+  @Get('summary')
+  async summary() {
+    const dashboard = await this.dashboardService.summary();
+
+    return {
+      message: 'Resumo do dashboard retornado com sucesso.',
+      data: dashboard,
+    };
+  }
+}
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { OrderStatus } from '@prisma/client';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+  constructor(private prisma: PrismaService) {}
+
+  async summary() {
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999);
+
+    // Pedidos do dia
+    const ordersToday = await this.prisma.order.findMany({
+      where: {
+        createdAt: {
+          gte: startOfDay,
+          lte: endOfDay,
+        },
+      },
+    });
+
+    // Receita do dia (não considera cancelados)
+    const revenueToday = ordersToday
+      .filter(order => order.status !== OrderStatus.CANCELED)
+      .reduce((total, order) => total + Number(order.total), 0);
+
+    return {
+      ordersToday: ordersToday.length,
+
+      revenueToday,
+
+      preparing: ordersToday.filter(
+        order => order.status === OrderStatus.PREPARING,
+      ).length,
+
+      ready: ordersToday.filter(
+        order => order.status === OrderStatus.READY,
+      ).length,
+
+      delivery: ordersToday.filter(
+        order => order.status === OrderStatus.OUT_FOR_DELIVERY,
+      ).length,
+
+      finished: ordersToday.filter(
+        order => order.status === OrderStatus.FINISHED,
+      ).length,
+
+      canceled: ordersToday.filter(
+        order => order.status === OrderStatus.CANCELED,
+      ).length,
+    };
+  }
+}
